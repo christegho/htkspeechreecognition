d5317cfbf30c49c46f425d784d17b7a8fbe93294
christegho <christegho@users.noreply.github.com>
2016-12-09 20:02:23 +0000

    Add files via upload

--- biphonescript.sh
+++ biphonescript.sh
@@ -1,12 +1,12 @@
 
 #Biphone generate with different TBVAL
-../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/MFC_E_D_A_Z_Flatstart20/mono hmm14 MFC_E_D_A_Z_Flatstart20/xwbir/RO100/TB4000
+../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/MFC_E_D_A_Z_Flatstart20/mono hmm14 MFC_E_D_A_Z_Flatstart20/xwbil/RO100/TB4000
 
-../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/MFC_E_D_A_Z_Init20/mono hmm14 MFC_E_D_A_Z_Init20/xwbir/RO100/TB4000
+../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/MFC_E_D_A_Z_Init20/mono hmm14 MFC_E_D_A_Z_Init20/xwbil/RO100/TB4000
 
-../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/FBK_D_A_Z_Flatstart20/mono hmm14 FBK_D_A_Z_Flatstart20/xwbir/RO100/TB4000
+../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/FBK_D_A_Z_Flatstart20/mono hmm14 FBK_D_A_Z_Flatstart20/xwbil/RO100/TB4000
 
-../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/FBK_D_A_Z_Init20/mono hmm14 FBK_D_A_Z_Init20/xwbir/RO100/TB4000
+../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/FBK_D_A_Z_Init20/mono hmm14 FBK_D_A_Z_Init20/xwbil/RO100/TB4000
 
 ../tools-bip/step-xbir -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/MFC_E_D_A_Z_Flatstart20/mono hmm14 MFC_E_D_A_Z_Flatstart20/xwbir/RO100/TB4000
 
@@ -16,13 +16,13 @@
 
 ../tools-bip/step-xbir -NUMMIXES 20 -ROVAL 100 -TBVAL 4000 \ $PWD/FBK_D_A_Z_Init20/mono hmm14 FBK_D_A_Z_Init20/xwbir/RO100/TB4000
 
-../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/MFC_E_D_A_Z_Flatstart20/mono hmm14 MFC_E_D_A_Z_Flatstart20/xwbir/RO100/TB2000
+../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/MFC_E_D_A_Z_Flatstart20/mono hmm14 MFC_E_D_A_Z_Flatstart20/xwbil/RO100/TB2000
 
-../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/MFC_E_D_A_Z_Init20/mono hmm14 MFC_E_D_A_Z_Init20/xwbir/RO100/TB2000
+../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/MFC_E_D_A_Z_Init20/mono hmm14 MFC_E_D_A_Z_Init20/xwbil/RO100/TB2000
 
-../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/FBK_D_A_Z_Flatstart20/mono hmm14 FBK_D_A_Z_Flatstart20/xwbir/RO100/TB2000
+../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/FBK_D_A_Z_Flatstart20/mono hmm14 FBK_D_A_Z_Flatstart20/xwbil/RO100/TB2000
 
-../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/FBK_D_A_Z_Init20/mono hmm14 FBK_D_A_Z_Init20/xwbir/RO100/TB2000
+../tools-bip/step-xbil -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/FBK_D_A_Z_Init20/mono hmm14 FBK_D_A_Z_Init20/xwbil/RO100/TB2000
 
 ../tools-bip/step-xbir -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/MFC_E_D_A_Z_Flatstart20/mono hmm14 MFC_E_D_A_Z_Flatstart20/xwbir/RO100/TB2000
 
@@ -32,7 +32,7 @@
 
 ../tools-bip/step-xbir -NUMMIXES 20 -ROVAL 100 -TBVAL 2000 \ $PWD/FBK_D_A_Z_Init20/mono hmm14 FBK_D_A_Z_Init20/xwbir/RO100/TB2000
 
-#TODO
+#In Progress
 #Decoding Biphones without options RIGHT 
 cd MLSALT2/pracgmm/exp
 for file in MFC_E_D_A_Z_Init20 MFC_E_D_A_Z_Flatstart20 FBK_D_A_Z_Init20 FBK_D_A_Z_Flatstart20
@@ -72,7 +72,7 @@ done
 done
 done
 
-#TODO
+#Done
 #Decoding Biphones without options LEFT 
 cd MLSALT2/pracgmm/exp
 for file in MFC_E_D_A_Z_Init20 MFC_E_D_A_Z_Flatstart20 FBK_D_A_Z_Init20 FBK_D_A_Z_Flatstart20
@@ -111,3 +111,157 @@ for file in MFC_E_D_A_Z_Init20 MFC_E_D_A_Z_Flatstart20 FBK_D_A_Z_Init20 FBK_D_A_
 done
 done
 done
+
+###################################################################
+#CD BI DNN -Varying Structure
+###################################################################
+#Done
+#Step Align
+cd ~/MLSALT2/pracgmm/exp
+
+for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 FBK_D_A_Z_Flatstart20 MFC_E_D_A_Z_Flatstart20
+do
+	../tools/steps/step-align $PWD/$file/xwbir/RO100/TB4000 hmm84 \ $file/align-bir-hmm84
+	../tools/steps/step-align $PWD/$file/xwbil/RO100/TB4000 hmm84 \ $file/align-bil-hmm84
+done
+
+
+for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 FBK_D_A_Z_Flatstart20 MFC_E_D_A_Z_Flatstart20
+do
+	../tools/steps/step-align $PWD/$file/xwbir/RO100/TB4000 hmm164 \ $file/align-bir-hmm164
+	../tools/steps/step-align $PWD/$file/xwbil/RO100/TB4000 hmm164 \ $file/align-bil-hmm164
+done
+
+#Dnn Train
+#In progress
+
+cd ~/MLSALT2/pracdnn/exp
+for N in 650
+do
+for H in 5 7
+do
+for file in MFC_E_D_A_Z_Init20 
+do
+pracgmmexp=~/MLSALT2/pracgmm/exp/$file
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-bir-hmm84/align/timit_train.mlf $pracgmmexp/xwbir/RO100/TB4000/hmm84/MMF \ $pracgmmexp/xwbir/RO100/TB4000/hmms.mlist MHO/dnntrainbir/hmm84/${file}_${H}H_CS4_N${N}
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-bil-hmm84/align/timit_train.mlf $pracgmmexp/xwbil/RO100/TB4000/hmm84/MMF \ $pracgmmexp/xwbil/RO100/TB4000/hmms.mlist MHO/dnntrainbil/hmm84/${file}_${H}H_CS4_N${N}
+
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-bir-hmm164/align/timit_train.mlf $pracgmmexp/xwbir/RO100/TB4000/hmm164/MMF \ $pracgmmexp/xwbir/RO100/TB4000/hmms.mlist MHO/dnntrainbir/hmm164/${file}_${H}H_CS4_N${N}
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-bil-hmm164/align/timit_train.mlf $pracgmmexp/xwbil/RO100/TB4000/hmm164/MMF \ $pracgmmexp/xwbil/RO100/TB4000/hmms.mlist MHO/dnntrainbil/hmm164/${file}_${H}H_CS4_N${N}
+
+
+done
+done
+done
+
+cd ~/MLSALT2/pracdnn/exp
+for N in 650
+do
+for H in 5 7
+do
+for file in  FBK_D_A_Z_Init20
+do
+pracgmmexp=~/MLSALT2/pracgmm/exp/$file
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/fbk25d/env/environment_D_A_Z \ $pracgmmexp/align-bir-hmm84/align/timit_train.mlf $pracgmmexp/xwbir/RO100/TB4000/hmm84/MMF \ $pracgmmexp/xwbir/RO100/TB4000/hmms.mlist MHO/dnntrainbir/hmm84/${file}_${H}H_CS4_N${N}
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/fbk25d/env/environment_D_A_Z \ $pracgmmexp/align-bil-hmm84/align/timit_train.mlf $pracgmmexp/xwbil/RO100/TB4000/hmm84/MMF \ $pracgmmexp/xwbil/RO100/TB4000/hmms.mlist MHO/dnntrainbil/hmm84/${file}_${H}H_CS4_N${N}
+
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/fbk25d/env/environment_D_A_Z \ $pracgmmexp/align-bir-hmm164/align/timit_train.mlf $pracgmmexp/xwbir/RO100/TB4000/hmm164/MMF \ $pracgmmexp/xwbir/RO100/TB4000/hmms.mlist MHO/dnntrainbir/hmm164/${file}_${H}H_CS4_N${N}
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/fbk25d/env/environment_D_A_Z \ $pracgmmexp/align-bil-hmm164/align/timit_train.mlf $pracgmmexp/xwbil/RO100/TB4000/hmm164/MMF \ $pracgmmexp/xwbil/RO100/TB4000/hmms.mlist MHO/dnntrainbil/hmm164/${file}_${H}H_CS4_N${N}
+
+
+done
+done
+done
+
+#Skipping
+for N in 500
+do
+for H in  5 7
+do
+for file in MFC_E_D_A_Z_Init20 FBK_D_A_Z_Init20
+do
+pracgmmexp=~/MLSALT2/pracgmm/exp/$file
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-tri-hmm84/align/timit_train.mlf $pracgmmexp/xwtri/RO100/TB4000/hmm84/MMF \ $pracgmmexp/xwtri/RO100/TB4000/hmms.mlist MHO/dnntraintri/hmm84/${file}_${H}H_CS4_N${N}
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-tri-hmm164/align/timit_train.mlf $pracgmmexp/xwtri/RO100/TB4000/hmm164/MMF \ $pracgmmexp/xwtri/RO100/TB4000/hmms.mlist MHO/dnntraintri/hmm164/${file}_${H}H_CS4_N${N}
+
+done
+done
+done
+done
+
+
+#test
+#TODO 
+for i in 5 7
+do
+H=$i
+dnnft=$(expr $i + 2)
+for N in 400 650
+do
+for hmm in  hmm84
+	do
+	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 
+	do	
+	echo $hmm ${file} -CS 4 -H $H -N $N >> dnntrainbil.txt
+	grep -A7 -P 'Epoch 12' MHO/dnntrainbil/$hmm/${file}_${H}H_CS4_N${N}/dnn${dnnft}.finetune/LOG >> dnntrainbil.txt
+	echo $hmm $file -CS 4 -H $H -N 500 >> dnntestbil.txt
+	echo $hmm $file -H $H -N $N 
+	../tools/steps/step-decode $PWD/MHO/dnntrainbil/$hmm/${file}_${H}H_CS4_N${N} dnn${dnnft}.finetune MHO/decode-bil-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}
+	grep -A2 -P 'SENT' MHO/decode-bil-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}/test/LOG >> dnntestbil.txt
+
+done
+done
+done
+done
+
+#TODO
+for i in 5 7
+do
+H=$i
+dnnft=$(expr $i + 2)
+for N in 400 650
+do
+for hmm in  hmm84
+	do
+	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 
+	do	
+	echo $hmm ${file} -CS 4 -H $H -N $N >> dnntrainbir.txt
+	grep -A7 -P 'Epoch 12' MHO/dnntrainbir/$hmm/${file}_${H}H_CS4_N${N}/dnn${dnnft}.finetune/LOG >> dnntrainbir.txt
+	echo $hmm $file -CS 4 -H $H -N 500 >> dnntestbir.txt
+	echo $hmm $file -H $H -N $N 
+	../tools/steps/step-decode $PWD/MHO/dnntrainbir/$hmm/${file}_${H}H_CS4_N${N} dnn${dnnft}.finetune MHO/decode-bir-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}
+	grep -A2 -P 'SENT' MHO/decode-bir-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}/test/LOG >> dnntestbir.txt
+
+done
+done
+done
+done
+
+
+#Skipping
+for i in 3 5 7
+do
+H=$i
+dnnft=$(expr $i + 2)
+for hmm in  hmm84
+	do
+	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20
+	do	
+	echo $hmm ${file} -CS 4 -H $H -N 500 >> dnntraintri.txt
+	grep -A7 -P 'Epoch 12' MHO/dnntraintri/$hmm/${file}_1H_CS${CS}/dnn${dnnft}.finetune/LOG >> dnntraintri.txt
+	echo $hmm $file -CS 4 -H $H -N 500 >> dnntesttri.txt
+	echo $hmm $file
+	../tools/steps/step-decode $PWD/MHO/dnntraintri/$hmm/${file}_${H}H_CS4 dnn${dnnft}.finetune MHO/decode-tri-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4
+	grep -A2 -P 'SENT' MHO/decode-tri-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4/test/LOG >> dnntesttri.txt
+done
+done
+done
+done
+
--- dnnscript.sh
+++ dnnscript.sh
@@ -85,7 +85,7 @@ pracgmmexp=~/MLSALT2/pracgmm/exp/$file
 done
 done
 
-#TODO
+#Done
 for CS in 3 4 5
 do
 for hmm in hmm164 hmm84
@@ -93,12 +93,12 @@ for hmm in hmm164 hmm84
 	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 FBK_D_A_Z_Flatstart20 MFC_E_D_A_Z_Flatstart20
 	do	
 	echo $hmm ${file} -CS 4 -H 1 -N 500 >> dnntrain.txt
-	grep -A7 -P 'Epoch 12' MHO/dnntrain/$hmm/${file}_1H_CS${CS}/dnn7.finetune/LOG >> dnntrain.txt
+	grep -A7 -P 'Epoch 12' MHO/dnntrain/$hmm/${file}_1H_CS${CS}/dnn3.finetune/LOG >> dnntrain.txt
 done
 done
 done
 
-#TODO
+#Done
 #Test model
 for CS in 3 4 5
 do
@@ -108,8 +108,8 @@ for hmm in hmm164 hmm84
 	do	
 	echo $hmm $file -CS $CS -H 1 -N 500 >> dnntest.txt
 	echo $hmm $file
-	../tools/steps/step-decode $PWD/MHO/dnntrain/$hmm/${file}_1H_CS${CS} dnn7.finetune MHO/decode-dnn7.finetune/$hmm/${file}_1H_CS${CS}
-	grep -A2 -P 'SENT' MHO/decode-dnn7.finetune/$hmm/${file}_1H_CS${CS}/test/LOG >> dnntest.txt
+	../tools/steps/step-decode $PWD/MHO/dnntrain/$hmm/${file}_1H_CS${CS} dnn3.finetune MHO/decode-dnn3.finetune/$hmm/${file}_1H_CS${CS}
+	grep -A2 -P 'SENT' MHO/decode-dnn3.finetune/$hmm/${file}_1H_CS${CS}/test/LOG >> dnntest.txt
 done
 done
 done
@@ -117,7 +117,7 @@ done
 ###################################################################
 #CI -Varying Structure
 ###################################################################
-#in prog
+#done
 for H in 3 7
 do
 for file in MFC_E_D_A_Z_Init20 FBK_D_A_Z_Init20
@@ -127,7 +127,7 @@ pracgmmexp=~/MLSALT2/pracgmm/exp/$file
 done
 done
 
-#in prog
+#done
 for N in 400 650
 do
 for file in MFC_E_D_A_Z_Init20 FBK_D_A_Z_Init20
@@ -140,38 +140,46 @@ pracgmmexp=~/MLSALT2/pracgmm/exp/$file
 done
 done
 
-#TODO
+#Done
 #test
-for H in 5 7
+for i in 5 7
+do
+H=$i
+dnnft=$(expr $i + 2)
 for N in 400 650
 do
 for hmm in  hmm84
 	do
-	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20
+	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 
 	do	
 	echo $hmm ${file} -CS 4 -H $H -N $N >> dnntrain.txt
-	grep -A7 -P 'Epoch 12' MHO/dnntrain/$hmm/${file}_${H}H_CS4_N${N}/dnn7.finetune/LOG >> dnntrain.txt
+	grep -A7 -P 'Epoch 12' MHO/dnntrain/$hmm/${file}_${H}H_CS4_N${N}/dnn${dnnft}.finetune/LOG >> dnntrain.txt
 	echo $hmm $file -CS 4 -H $H -N 500 >> dnntest.txt
-	echo $hmm $file
-	../tools/steps/step-decode $PWD/MHO/dnntrain/$hmm/${file}_${H}H_CS4_N${N} dnn7.finetune MHO/decode-dnn7.finetune/$hmm/${file}_7H_CS4_N${N}
-	grep -A2 -P 'SENT' MHO/decode-dnn7.finetune/$hmm/${file}_${H}H_CS4_N${N}/test/LOG >> dnntest.txt
+	echo $hmm $file -H $H -N $N 
+	../tools/steps/step-decode $PWD/MHO/dnntrain/$hmm/${file}_${H}H_CS4_N${N} dnn${dnnft}.finetune MHO/decode-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}
+	grep -A2 -P 'SENT' MHO/decode-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}/test/LOG >> dnntest.txt
+
+done
 done
 done
 done
 
-#TODO
-for H in 3 7
+#DONE
+for i in 3 7
 do
+H=$i
+dnnft=$(expr $i + 2)
 for hmm in  hmm84
 	do
 	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20
 	do	
 	echo $hmm ${file} -CS 4 -H $H -N 500 >> dnntrain.txt
-	grep -A7 -P 'Epoch 12' MHO/dnntrain/$hmm/${file}_1H_CS${CS}/dnn7.finetune/LOG >> dnntrain.txt
+	grep -A7 -P 'Epoch 12' MHO/dnntrain/$hmm/${file}_1H_CS${CS}/dnn${dnnft}.finetune/LOG >> dnntrain.txt
 	echo $hmm $file -CS 4 -H $H -N 500 >> dnntest.txt
 	echo $hmm $file
-	../tools/steps/step-decode $PWD/MHO/dnntrain/$hmm/${file}_${H}H_CS4 dnn7.finetune MHO/decode-dnn7.finetune/$hmm/${file}_${H}H_CS4
-	grep -A2 -P 'SENT' MHO/decode-dnn7.finetune/$hmm/${file}_${H}H_CS4/test/LOG >> dnntest.txt
+	../tools/steps/step-decode $PWD/MHO/dnntrain/$hmm/${file}_${H}H_CS4 dnn${dnnft}.finetune MHO/decode-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4
+	grep -A2 -P 'SENT' MHO/decode-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4/test/LOG >> dnntest.txt
+done
 done
 done
 done
@@ -179,3 +187,100 @@ done
 ###################################################################
 #CD TRI -Varying Structure
 ###################################################################
+#Done
+#Step Align
+cd ~/MLSALT2/pracgmm/exp
+
+for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 FBK_D_A_Z_Flatstart20 MFC_E_D_A_Z_Flatstart20
+do
+	../tools/steps/step-align $PWD/$file/xwtri/RO100/TB4000 hmm84 \ $file/align-tri-hmm84
+done
+
+
+for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 FBK_D_A_Z_Flatstart20 MFC_E_D_A_Z_Flatstart20
+do
+	../tools/steps/step-align $PWD/$file/xwtri/RO100/TB4000 hmm164 \ $file/align-tri-hmm164
+done
+
+#Dnn Train
+#Done
+for N in 400 650
+do
+for H in 5 7
+do
+for file in MFC_E_D_A_Z_Init20 FBK_D_A_Z_Init20
+do
+pracgmmexp=~/MLSALT2/pracgmm/exp/$file
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-tri-hmm84/align/timit_train.mlf $pracgmmexp/xwtri/RO100/TB4000/hmm84/MMF \ $pracgmmexp/xwtri/RO100/TB4000/hmms.mlist MHO/dnntraintri/hmm84/${file}_${H}H_CS4_N${N}
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4N${N}  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-tri-hmm164/align/timit_train.mlf $pracgmmexp/xwtri/RO100/TB4000/hmm164/MMF \ $pracgmmexp/xwtri/RO100/TB4000/hmms.mlist MHO/dnntraintri/hmm164/${file}_${H}H_CS4_N${N}
+
+done
+done
+done
+
+#Done
+for N in 500
+do
+for H in 3 5 7
+do
+for file in MFC_E_D_A_Z_Init20 FBK_D_A_Z_Init20
+do
+pracgmmexp=~/MLSALT2/pracgmm/exp/$file
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-tri-hmm84/align/timit_train.mlf $pracgmmexp/xwtri/RO100/TB4000/hmm84/MMF \ $pracgmmexp/xwtri/RO100/TB4000/hmms.mlist MHO/dnntraintri/hmm84/${file}_${H}H_CS4_N${N}
+
+../tools/steps/step-dnntrain -DNNTRAINHTE ~/MLSALT2/pracdnn/exp/HTEdnntrain${H}HCS4  ../convert/mfc13d/env/environment_E_D_A_Z \ $pracgmmexp/align-tri-hmm164/align/timit_train.mlf $pracgmmexp/xwtri/RO100/TB4000/hmm164/MMF \ $pracgmmexp/xwtri/RO100/TB4000/hmms.mlist MHO/dnntraintri/hmm164/${file}_${H}H_CS4_N${N}
+
+done
+done
+done
+done
+
+
+#test
+#Done
+for i in 5 7
+do
+H=$i
+dnnft=$(expr $i + 2)
+for N in 400 650
+do
+for hmm in  hmm84
+	do
+	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20 
+	do	
+	echo $hmm ${file} -CS 4 -H $H -N $N >> dnntraintri.txt
+	grep -A7 -P 'Epoch 12' MHO/dnntraintri/$hmm/${file}_${H}H_CS4_N${N}/dnn${dnnft}.finetune/LOG >> dnntraintri.txt
+	echo $hmm $file -CS 4 -H $H -N 500 >> dnntesttri.txt
+	echo $hmm $file -H $H -N $N 
+	../tools/steps/step-decode $PWD/MHO/dnntraintri/$hmm/${file}_${H}H_CS4_N${N} dnn${dnnft}.finetune MHO/decode-tri-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}
+	grep -A2 -P 'SENT' MHO/decode-tri-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}/test/LOG >> dnntesttri.txt
+
+done
+done
+done
+done
+
+#In progress #TODO check if done properly
+for N in 500
+do
+for i in 3 5 7
+do
+H=$i
+dnnft=$(expr $i + 2)
+for hmm in  hmm84
+	do
+	for file in MFC_E_D_A_Z_Init20  FBK_D_A_Z_Init20
+	do	
+	echo $hmm ${file} -CS 4 -H $H -N 500 >> dnntraintri.txt
+	grep -A7 -P 'Epoch 12' MHO/dnntraintri/$hmm/${file}_1H_CS4_N${N}/dnn${dnnft}.finetune/LOG >> dnntraintri.txt
+	echo $hmm $file -CS 4 -H $H -N 500 >> dnntesttri.txt
+	echo $hmm $file
+	../tools/steps/step-decode $PWD/MHO/dnntraintri/$hmm/${file}_${H}H_CS4_N${N} dnn${dnnft}.finetune MHO/decode-tri-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}
+	grep -A2 -P 'SENT' MHO/decode-tri-dnn${dnnft}.finetune/$hmm/${file}_${H}H_CS4_N${N}/test/LOG >> dnntesttri.txt
+done
+done
+done
+done
+done
+
